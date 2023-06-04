b3d84fc1fa6f6ee50727f0dcdbe9cf12fd208e6c
PedroHLongo <pedro.longo@bradescoseguros.com.br>
2023-06-04 14:01:31 -0300

    Solved weight exercise

---
+++ src/classAndMethods/tests/DataTest.java
@@ -0,0 +1,14 @@
+package classAndMethods.tests;
+
+import classAndMethods.Data;
+
+public class DataTest {
+    public static void main(String[] args) {
+        Data date = new Data();
+        date.day = 17;
+        date.month = 7;
+        date.year = 2023;
+
+        System.out.printf("Day: %d, Month: %d, Year: %d", date.day, date.month, date.year);
+    }
+}
