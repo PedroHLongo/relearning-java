967dfa5ca1b489f818cd18d6dbb918b94f57463c
PedroHLongo <pedro.longo@bradescoseguros.com.br>
2023-10-01 15:30:40 -0300

    Learned predicate

---
+++ src/lambdas/functionalinterfaces/predicate/Predicado.java
@@ -0,0 +1,14 @@
+package lambdas.functionalinterfaces.predicate;
+
+import java.util.function.Predicate;
+
+public class Predicado {
+    public static void main(String[] args) {
+        Predicate<Product> isExpensive =
+                prd -> (prd.getPrice() * (1 - prd.getDiscount())) >= 750;
+
+        Product product = new Product("Notebook", 3989.50, 0.15);
+
+        System.out.println(isExpensive.test(product));
+    }
+}
