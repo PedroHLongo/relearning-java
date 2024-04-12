12a03e4328a9fdc349029be9bf4e3a639309bbf3
PedroHLongo <phenriquelongo@gmail.com>
2024-04-12 09:57:07 -0300

    Deleting old files

--- src/classAndMethods/Data.java
+++
@@ -1,7 +0,0 @@
-package classAndMethods;
-
-public class Data {
-    public Integer day;
-    public Integer month;
-    public Integer year;
-}
--- src/oop/inheritance/SportCar.java
+++
@@ -1,6 +0,0 @@
-package oop.inheritance;
-
-public interface SportCar {
-    void turnOnTurbo();
-    void turnOffTurbo();
-}
