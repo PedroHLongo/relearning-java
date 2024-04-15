3b9f0bc87276b86395f8eef30221cb2a4414e394
PedroHLongo <phenriquelongo@gmail.com>
2024-04-15 15:24:44 -0300

    Create get user service

--- src/study/service/GetUserService.java
+++ src/study/service/GetUserService.java
@@ -6,16 +6,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class InsertUser {
-
+public class GetUserService {
     public static void main(String[] args) {
         final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa-class");
         final EntityManager entityManager = entityManagerFactory.createEntityManager();
-        final User user = new User("Pedro", "pedro@gmail.com");
 
-        entityManager.getTransaction().begin();
-        entityManager.persist(user);
-        entityManager.getTransaction().commit();
+        User user = entityManager.find(User.class, 1L);
+        System.out.println(user);
 
         entityManager.close();
         entityManagerFactory.close();
---
+++ src/study/service/InsertUserService.java
@@ -0,0 +1,23 @@
+package study.service;
+
+import study.entities.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class InsertUserService {
+
+    public static void main(String[] args) {
+        final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa-class");
+        final EntityManager entityManager = entityManagerFactory.createEntityManager();
+        final User user = new User("Pedro", "pedro@gmail.com");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(user);
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+}
