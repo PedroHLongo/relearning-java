6d454efbfcc0ccfe927a9c977114b78d4dd330a9
PedroHLongo <phenriquelongo@gmail.com>
2024-06-06 22:07:25 -0300

    Create repository using spring data jpa

--- spring-boot-02/src/main/java/com/springboot/spring_boot_02/CourseCommandLineRunner.java
+++ spring-boot-02/src/main/java/com/springboot/spring_boot_02/CourseCommandLineRunner.java
@@ -1,28 +1,44 @@
-package com.springboot.spring_boot_02.jdbc;
+package com.springboot.spring_boot_02;
 
-import com.springboot.spring_boot_02.Course;
+import com.springboot.spring_boot_02.jdbc.CourseJdbcRepository;
+import com.springboot.spring_boot_02.jpa.CourseJpaRepository;
+import com.springboot.spring_boot_02.springdata.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+public class CourseCommandLineRunner implements CommandLineRunner {
 
     @Autowired
-    private CourseJdbcRepository repository;
+    private CourseJdbcRepository jdbcRepository;
+
+    @Autowired
+    private CourseJpaRepository jpaRepository;
+
+    @Autowired
+    private CourseSpringDataJpaRepository springDataJpaRepository;
 
     @Override
     public void run(String... args) throws Exception {
         final Course springBootCourse = new Course(1L, "Learn spring boot", "In28Minutes");
         final Course springBootFramework = new Course(2L, "Learn spring framework", "In28Minutes");
 
-        repository.insert(springBootCourse);
-        repository.insert(springBootFramework);
+        jpaRepository.insert(springBootCourse);
+        jpaRepository.insert(springBootFramework);
 
         System.out.println("Insert ok");
 
-        repository.delete(springBootCourse);
+        jpaRepository.deleteById(springBootCourse.getId());
 
-        System.out.println(repository.findById(2L));
+        System.out.println(jpaRepository.findById(2L));
+
+        final Course springDataCourse1 = new Course(3L, "Learn spring data jpa", "In28Minutes");
+        final Course springDataCourse2 = new Course(4L, "Learn spring data jpa2", "In28Minutes");
+
+        springDataJpaRepository.save(springDataCourse1);
+        springDataJpaRepository.save(springDataCourse2);
+
+        springDataJpaRepository.delete(springDataCourse1);
     }
 }
