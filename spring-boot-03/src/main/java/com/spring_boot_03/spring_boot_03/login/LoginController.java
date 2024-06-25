5111abae6def5f6b8f25f58dbd3e8773fd8bc00d
PedroHLongo <phenriquelongo@gmail.com>
2024-06-25 19:35:35 -0300

    Separate get from post in loggin controller

--- spring-boot-03/src/main/java/com/spring_boot_03/spring_boot_03/login/LoginController.java
+++ spring-boot-03/src/main/java/com/spring_boot_03/spring_boot_03/login/LoginController.java
@@ -4,23 +4,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class LoginController {
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping("/login")
-    public String sayHelloJsp(@RequestParam final String name,
-                              @RequestParam final String password,
-                              final ModelMap model) {
-        model.put("name", name);
-        logger.debug("Request param: {}", name);
-        System.out.println("NAME -> " + name);
-        System.out.println("PASSWORD  -> " + password);
+    @RequestMapping(value="/login", method=RequestMethod.GET)
+    public String sayHelloJsp() {
         return "login";
+    }
+
+    @RequestMapping(value="/login", method=RequestMethod.POST)
+    public String gotoWelcomePage(@RequestParam final String name,
+                                  @RequestParam final String password,
+                                  final ModelMap modelMap) {
+        modelMap.put("name", name);
+        System.out.println("NAME -> " + name);
+        modelMap.put("password", password);
+        System.out.println("PASSWORD -> " + password);
+        return "welcome";
     }
 }
---
+++ spring-boot-03/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Welcome page</title>
+    </head>
+    <body>
+        <div>Welcome to the login page!</div>
+    </body>
+</html>
