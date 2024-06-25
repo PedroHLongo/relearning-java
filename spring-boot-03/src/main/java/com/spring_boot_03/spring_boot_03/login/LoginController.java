d555eba086410274a435bebb4fd52d8de962cca5
PedroHLongo <phenriquelongo@gmail.com>
2024-06-25 19:10:19 -0300

    Adding login form

--- spring-boot-03/src/main/java/com/spring_boot_03/spring_boot_03/login/LoginController.java
+++ spring-boot-03/src/main/java/com/spring_boot_03/spring_boot_03/login/LoginController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -15,31 +14,13 @@ public class LoginController {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("/login")
-    @ResponseBody
-    public String sayHello() {
-        return "Login!";
-    }
-
-    @RequestMapping("/login-html")
-    @ResponseBody
-    public String sayHelloHtml() {
-        final StringBuffer stringBuffer = new StringBuffer();
-        stringBuffer.append("<html>");
-        stringBuffer.append("<head>");
-        stringBuffer.append("<title>Login page</title>");
-        stringBuffer.append("</head>");
-        stringBuffer.append("<body>");
-        stringBuffer.append("Login page!");
-        stringBuffer.append("</body>");
-        stringBuffer.append("</html>");
-
-        return stringBuffer.toString();
-    }
-
-    @RequestMapping("/login-jsp")
-    public String sayHelloJsp(@RequestParam final String name, final ModelMap model) {
+    public String sayHelloJsp(@RequestParam final String name,
+                              @RequestParam final String password,
+                              final ModelMap model) {
         model.put("name", name);
         logger.debug("Request param: {}", name);
+        System.out.println("NAME -> " + name);
+        System.out.println("PASSWORD  -> " + password);
         return "login";
     }
 }
