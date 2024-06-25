0e2f1cb64c94bf73e49fd7f23e3b8d0bf83dae58
PedroHLongo <phenriquelongo@gmail.com>
2024-06-25 18:58:43 -0300

    Adding logging to login controller

--- spring-boot-03/src/main/java/com/spring_boot_03/spring_boot_03/login/LoginController.java
+++ spring-boot-03/src/main/java/com/spring_boot_03/spring_boot_03/login/LoginController.java
@@ -1,13 +1,18 @@
 package com.spring_boot_03.spring_boot_03.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
 public class LoginController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("/login")
     @ResponseBody
@@ -34,7 +39,7 @@ public class LoginController {
     @RequestMapping("/login-jsp")
     public String sayHelloJsp(@RequestParam final String name, final ModelMap model) {
         model.put("name", name);
-        System.out.println("NOMEEEE -> " + name);
+        logger.debug("Request param: {}", name);
         return "login";
     }
 }
