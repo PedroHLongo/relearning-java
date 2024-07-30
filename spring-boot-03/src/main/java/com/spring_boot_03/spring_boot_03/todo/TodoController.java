5d9018777ccf282c78804816ab6b245a5907668e
PedroHLongo <phenriquelongo@gmail.com>
2024-07-29 21:18:15 -0300

    Create logic to add new todo from webpage

--- spring-boot-03/src/main/java/com/spring_boot_03/spring_boot_03/todo/TodoController.java
+++ spring-boot-03/src/main/java/com/spring_boot_03/spring_boot_03/todo/TodoController.java
@@ -1,11 +1,14 @@
 package com.spring_boot_03.spring_boot_03.todo;
 
+import org.eclipse.tags.shaded.org.apache.xpath.operations.Bool;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -20,7 +23,7 @@ public class TodoController {
 
     @RequestMapping(value= "/todos", method = RequestMethod.GET)
     public String todos(final ModelMap modelMap) {
-        final List<Todo> todos = todoService.findByUser("pedro");
+        final List<Todo> todos = todoService.findByUser("Pedro");
         modelMap.put("todos", todos);
         return "listTodos";
     }
@@ -33,7 +36,8 @@ public class TodoController {
 
 
     @RequestMapping(value= "/add-todo", method = RequestMethod.POST)
-    public String addNewTodo() {
+    public String addNewTodo(@RequestParam String description, ModelMap model) {
+        todoService.addTodo(model.get("username").toString(), description, LocalDate.now().plusYears(1), Boolean.FALSE);
         return "redirect:todos";
     }
 }
--- spring-boot-03/src/main/java/com/spring_boot_03/spring_boot_03/todo/TodoService.java
+++ spring-boot-03/src/main/java/com/spring_boot_03/spring_boot_03/todo/TodoService.java
@@ -9,13 +9,18 @@ import java.util.List;
 @Service
 public class TodoService {
     private static final List<Todo> todos = new ArrayList<>();
+    private static long todoCount = 0L;
     static {
-        todos.add(new Todo(1L, "pedro", "todo 1", LocalDate.now().plusDays(1L), Boolean.FALSE));
-        todos.add(new Todo(2L, "pedro", "todo 2", LocalDate.now().plusDays(2L), Boolean.FALSE));
-        todos.add(new Todo(3L, "pedro", "todo 3", LocalDate.now().plusDays(3L), Boolean.FALSE));
+        todos.add(new Todo(++todoCount, "Pedro", "todo 1", LocalDate.now().plusDays(1L), Boolean.FALSE));
+        todos.add(new Todo(++todoCount,"Pedro", "todo 2", LocalDate.now().plusDays(2L), Boolean.FALSE));
+        todos.add(new Todo(++todoCount,"Pedro", "todo 3", LocalDate.now().plusDays(3L), Boolean.FALSE));
     }
 
     public List<Todo> findByUser(final String user) {
         return todos.stream().filter(todo -> user.equals(todo.getUsername())).toList();
+    }
+
+    void addTodo(final String username, final String description, final LocalDate localDate, Boolean done) {
+        todos.add(new Todo(++todoCount, username, description, localDate, done));
     }
 }
